4ad6afe989ac1da8e6d8e3c27c6b06f514e15f50
Abdul Kholik <akl859349@gmail.com>
2025-04-23 21:07:44 +0700

    Update application name and welcome text to reflect new branding
    - Changed application name from 'Laravel' to 'Desa Digital' in configuration and layout files.
    - Updated welcome view text to provide a localized greeting for users.
    - Modified login and registration views to use a new logo and maintain consistent branding.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,7 +10,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Desa Digital') }}</title>
 
         <meta name="description" content="" />
 
